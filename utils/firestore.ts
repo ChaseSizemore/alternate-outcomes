c4d0d17401d49a4fe3576c6856f5dbf719ea2c7e
ChaseSizemore <ChaseLSizemore@gmail.com>
2023-10-10 01:26:30 -0400

    git ignore

--- utils/firestore.ts
+++ utils/firestore.ts
@@ -4,15 +4,15 @@ import { getAnalytics } from 'firebase/analytics';
 import { getFirestore, initializeFirestore } from 'firebase/firestore';
 import { FirebaseApp } from 'firebase/app';
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY, // Changed from FIRESTORE_API_KEY
-//   authDomain: process.env.FIREBASE_AUTHDOMAIN, // Changed from FIRESTORE_AUTH_DOMAIN
-//   projectId: process.env.FIREBASE_PROJECT_ID, // Remains the same
-//   storageBucket: process.env.FIREBASE_STOREBUCKET, // Changed from FIRESTORE_STOREBUCKET
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID, // Changed from FIRESTORE_MESSAGING_SENDER_ID
-//   appId: process.env.FIREBASE_APP_ID, // Changed from FIRESTORE_APP_ID
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID, // Changed from FIRESTORE_MEASUREMENT_ID
-// };
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY, // Changed from FIRESTORE_API_KEY
+  authDomain: process.env.FIREBASE_AUTHDOMAIN, // Changed from FIRESTORE_AUTH_DOMAIN
+  projectId: process.env.FIREBASE_PROJECT_ID, // Remains the same
+  storageBucket: process.env.FIREBASE_STOREBUCKET, // Changed from FIRESTORE_STOREBUCKET
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID, // Changed from FIRESTORE_MESSAGING_SENDER_ID
+  appId: process.env.FIREBASE_APP_ID, // Changed from FIRESTORE_APP_ID
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID, // Changed from FIRESTORE_MEASUREMENT_ID
+};
 
 
 
